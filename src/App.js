f3270ffc1011feda87f0c3c2d4741ce88b4c6490
Louis Faucon <lpfaucon@gmail.com>
2019-05-03 20:00:28 +0200

    Implement random card side

--- src/App.js
+++ src/App.js
@@ -45,14 +45,16 @@ const Controls = ({ turned, turn, next, answer, cards, cardIndex }) => {
 
 const FlashCard = ({ turned, card }) => {
   const [front, back] = (card && [card.front, card.back]) || ["", ""];
+  const reverse = card && new Date(card.lastSeenAt).getSeconds() % 2 > 0.5;
+  const [f, b] = reverse ? [front, back] : [back, front];
   return (
     <div id="flashcard">
       <div className={turned ? "showFront" : "showBack"}>
         <div className="front">
-          <span>{front}</span>
+          <span>{f}</span>
         </div>
         <div className="back">
-          <span>{back}</span>
+          <span>{b}</span>
         </div>
       </div>
     </div>
@@ -129,14 +131,14 @@ class App extends Component {
       setTimeout(() => this.setState({ cardIndex }), 500);
       return {
         turned: false,
-        cardIndex: undefined,
-        answered: false
+        cardIndex: undefined
       };
     });
 
   handleRecall = wasCorrect => {
     const { cards, cardIndex } = this.state;
     const newCards = [...cards];
+    if (!newCards[cardIndex]) return;
     newCards[cardIndex].lastSeenAt = new Date();
     if (wasCorrect) {
       newCards[cardIndex].count = (newCards[cardIndex].count || 1) + 1;
@@ -151,12 +153,12 @@ class App extends Component {
   toggleConfig = () => this.setState(s => ({ configOpen: !s.configOpen }));
 
   render() {
-    const { configOpen, cards, cardIndex } = this.state;
+    const { configOpen, cards, cardIndex, turned } = this.state;
     return (
       <div className="App">
         <Header openConfig={this.toggleConfig} />
         {!configOpen && (
-          <FlashCard {...this.state} card={cards && cards[cardIndex]} />
+          <FlashCard turned={turned} card={cards && cards[cardIndex]} />
         )}
         {!configOpen && (
           <Controls
