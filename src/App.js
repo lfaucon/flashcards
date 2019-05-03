8e1147aa7e4ca068f8cd27ce95883f7dcd0a20c6
Louis Faucon <lpfaucon@gmail.com>
2019-05-03 17:11:54 +0200

    Version 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,146 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState } from "react";
+import "./App.css";
+
+const Controls = ({ turned, turn, next, answer, cards }) => {
+  if (!cards || cards.length === 0) {
+    return (
+      <div id="controls">
+        <button className="button">You have no card yet</button>
+      </div>
+    );
+  }
+  return (
+    <div id="controls">
+      {!turned && (
+        <button className="button" onClick={turn}>
+          SHOW
+        </button>
+      )}
+      {turned && (
+        <button
+          className="button"
+          style={{ backgroundColor: "green" }}
+          onClick={() => answer(true) || next()}
+        >
+          I KNEW
+        </button>
+      )}
+      {turned && (
+        <button
+          className="button"
+          style={{ backgroundColor: "red" }}
+          onClick={() => answer(false) || next()}
+        >
+          I FORGOT
+        </button>
+      )}
+    </div>
+  );
+};
+
+const FlashCard = ({ turned, card }) => (
+  <div id="flashcard">
+    <div className={turned ? "showFront" : "showBack"}>
+      <div className="front">
+        <span>{card && card.front}</span>
+      </div>
+      <div className="back">
+        <span>{card && card.back}</span>
+      </div>
+    </div>
+  </div>
+);
+
+const Header = ({ openConfig }) => (
+  <div id="header">
+    <span>Flashcards</span>
+    <i className="fas fa-plus" onClick={openConfig} />
+  </div>
+);
+
+const getCards = config => {
+  try {
+    const cards = localStorage.getItem("lpfaucon.flashcards.cards");
+    return cards ? JSON.parse(cards) : [];
+  } catch {
+    return [];
+  }
+};
+
+const ConfigPanel = ({ toggleConfig, cards }) => {
+  const [front, setFront] = useState("");
+  const [back, setBack] = useState("");
+
+  const addCard = () => {
+    if (front === "" || back === "") return;
+    const newCard = { front, back };
+    const newCards = [...cards, newCard];
+    localStorage.setItem("lpfaucon.flashcards.cards", JSON.stringify(newCards));
+    toggleConfig();
+  };
+  return (
+    <div id="config">
+      <span>Front:</span>
+      <textarea value={front} onChange={e => setFront(e.target.value)} />
+      <span>Back:</span>
+      <textarea value={back} onChange={e => setBack(e.target.value)} />
+      <button
+        className="button"
+        style={{ marginTop: "16px" }}
+        onClick={addCard}
+      >
+        ADD
+      </button>
+    </div>
+  );
+};
 
 class App extends Component {
+  state = {
+    turned: false,
+    configOpen: false
+  };
+
+  componentDidMount() {
+    const cards = getCards();
+    const randIndex = Math.floor(Math.random() * cards.length);
+    this.setState({
+      cards,
+      card: cards[randIndex]
+    });
+  }
+
+  next = () =>
+    this.setState(s => {
+      const randIndex = Math.floor(Math.random() * s.cards.length);
+      setTimeout(() => this.setState({ card: s.cards[randIndex] }), 500);
+      return {
+        turned: false,
+        card: undefined,
+        answered: false
+      };
+    });
+
+  turn = () => this.setState(s => ({ turned: !s.turned }));
+  toggleConfig = () => this.setState(s => ({ configOpen: !s.configOpen }));
+
   render() {
+    const { configOpen } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header openConfig={this.toggleConfig} />
+        {!configOpen && <FlashCard {...this.state} />}
+        {!configOpen && (
+          <Controls
+            {...this.state}
+            turn={this.turn}
+            next={this.next}
+            answer={wasCorrect => {}}
+          />
+        )}
+        {configOpen && (
+          <ConfigPanel {...this.state} toggleConfig={this.toggleConfig} />
+        )}
       </div>
     );
   }
---
+++ src/translations.js
@@ -0,0 +1,219 @@
+export default [
+  "Eighteen 			".split("\t"),
+  "Twenty five			".split("\t"),
+  "Two hundred thirty four			".split("\t"),
+  "One thousand			".split("\t"),
+  "Today			".split("\t"),
+  "Tomorrow			".split("\t"),
+  "Yesterday			".split("\t"),
+  "Week			".split("\t"),
+  "Month			".split("\t"),
+  "Next week			".split("\t"),
+  "Last month			".split("\t"),
+  "Next weekend			".split("\t"),
+  "Morning			".split("\t"),
+  "Evening			".split("\t"),
+  "Afternoon			".split("\t"),
+  "Let's meet on Friday			".split("\t"),
+  "Monday	Lundi		".split("\t"),
+  "Tuesday	Mardi		".split("\t"),
+  "Wednesday	Mercredi		".split("\t"),
+  "Thursday	Vendredi		".split("\t"),
+  "Friday	Jeudi		".split("\t"),
+  "Saturday	Samedi		".split("\t"),
+  "Sunday	Dimache		".split("\t"),
+  "One	Un	หนึ่ง	Neng".split("\t"),
+  "Two	Deux		Song".split("\t"),
+  "Three	Trois		Sam".split("\t"),
+  "Four	Quatre		Si".split("\t"),
+  "Five	Cinq		Ah".split("\t"),
+  "Six	Six		".split("\t"),
+  "Seven	Sept		".split("\t"),
+  "Eight	Huit		".split("\t"),
+  "Nine	Neuf		".split("\t"),
+  "Ten	Dix		".split("\t"),
+  "Twenty	Vingt		".split("\t"),
+  "One Hundred	Cent		".split("\t"),
+  "You	Tu	คุณ	Khuun".split("\t"),
+  "He	Il	เขา	Khao".split("\t"),
+  "She	Elle	เธอ	Ther".split("\t"),
+  "I am Nice	Je suis gentille (f) / gentil (m)	ผมใจดี	Pom cai dii".split("\t"),
+  "I am not tall	Je ne suis pas grande (f) / grand (m)	ผมไม่สูง	Pom mai suung".split(
+    "\t"
+  ),
+  "You are beautiful	Tu es belle (f) / beau (m)	คุณสวย	Khuun suai".split("\t"),
+  "You are not cute	Tu n'es pas mignon	คุณไม่น่ารัก	Khuun mai na ruk".split("\t"),
+  "He is not handsome	Il n'est pas beau	เขาไม่หล่อ	Khao mai laoo".split("\t"),
+  "She is fat	Elle est grosse 	เธออ้วน	Ther ouen".split("\t"),
+  "A grey cat	Un chat gris	แมวสีเทา (หนึ่งตัว)	Maeo si tao (neng toa)".split("\t"),
+  "A white chicken	Un poulet blanc	ไก่สีขาว	Kai si kaao".split("\t"),
+  "A white dog	Un chien blanc	หมาสีขาว	Ma si kaao".split("\t"),
+  "A red flower	Une fleur rose	ดอกไม้สีแดง	Nok maii si deang".split("\t"),
+  "A red fruit 	Un fruit rouge	ผลไม้สีแดง	Poon le maii si daeng".split("\t"),
+  "Red	Rouge	สีแดง	Si Deang".split("\t"),
+  "Yellow	Jaune	สีเหลือง	Si Luang".split("\t"),
+  "Young	Jeune	เด็ก, อ่อนวัย	Dek, On wai".split("\t"),
+  "Blue	Bleu	สีฟ้า	Si faa".split("\t"),
+  "Green	Vert	สีเขียว	Si kiao".split("\t"),
+  "Black	Noir	สีดำ	Si daam".split("\t"),
+  "white	Blanc	สีขาว	Si kaao".split("\t"),
+  "Pink	Rose	สีชมพู	Si chompuu".split("\t"),
+  "Purple	Violet	สีม่วง	Si muang".split("\t"),
+  "Orange	Orange	สีส้ม	Si som".split("\t"),
+  "Grey	Gris	สีเทา	Si tao".split("\t"),
+  "Beautiful	Belle / Jolie 	สวย	Suai".split("\t"),
+  "Big	Grand	ใหญ่	Yai".split("\t"),
+  "Small	Petit	เล็ก	Lek".split("\t"),
+  "Nice	Gentil	ใจดี	Cai dii".split("\t"),
+  "Obese	Grosse (f) / gros (m)	อ้วน	Ouen".split("\t"),
+  "Skinny	Maigre	บาง	Baang".split("\t"),
+  "He has blue eye	Il a les yeux bleus	เขาตาสีฟ้า	Khao ta si fa".split("\t"),
+  "He doesn't have blue eyes	Il n'a pas les yeux bleus	เขาไม่ได้มีตาสีฟ้า	Khao mai daii mi ta si fa".split(
+    "\t"
+  ),
+  "She is tall	Elle est grande	เธอสูง	The suung".split("\t"),
+  "What?	Quoi?	อะไร	A-rai na?".split("\t"),
+  "Where are the toilets	Où sont les toilettes	ห้องน้ำอยู่ที่ไหน	Hong Nam Yuu Nhai".split(
+    "\t"
+  ),
+  "I am pooping	Je fais caca	ผมอึอยู่	Pom Eu Yuu".split("\t"),
+  "I need to pee	Je dois faire pipi	ผมต้องไปฉี่	Pom Tong Pai Qhii".split("\t"),
+  "There is no more toilet paper	Il n’y a plus de papier toilette	ในห้องน้ำไม่เหลือทิชชู่แล้ว	Nhai hong naam mai lue thi chu leaw".split(
+    "\t"
+  ),
+  "To brush	Brosser	แปรง	Pleng".split("\t"),
+  "I brush my teeth	Je brosse mes dents	ผมแปรงฟัน	Pom Pleng Fun".split("\t"),
+  "Toothpaste	Dentifrice	ยาสีฟัน	Ya si fan".split("\t"),
+  "To take	Prendre		".split("\t"),
+  "I take	Je prends		".split("\t"),
+  "He takes	Il prend		".split("\t"),
+  "We take	Nous prennons		".split("\t"),
+  "Can I take the toothpaste?	Est ce que je peux prendre le dentifrice?	ขอยาสีฟันหน่อยได้มั้ยครับ	Khor ya si fan noii daii mai khrap".split(
+    "\t"
+  ),
+  "Soap	Le savon	สบู่	Sabuu".split("\t"),
+  "Shampoo	Le shampoing	แชมพู	Shampoo".split("\t"),
+  "Bathroom	Salle de bain	ห้องน้ำ	Hong Nham".split("\t"),
+  "I take a shower	Je prends une douche	ผมอาบน้ำ	Pom ab-naam".split("\t"),
+  "A bath	Un bain	อาบน้ำ	Ab naam".split("\t"),
+  "I want to take a bath	Je veux prendre un bain	ผมอยากอาบน้ำ	Pom yaag ab naam".split(
+    "\t"
+  ),
+  "Cleaning myself	Se laver	ผมอาบน้ำ	Pom ab naam".split("\t"),
+  "I clean myself in the evening not the morning	Je me lave le soir mais pas le matin	ผมอาบน้ำตอนเย็นแต่ไม่ใช่ตอนเช้า	".split(
+    "\t"
+  ),
+  "I learn 10 new words every day	J'apprends 10 nouveau mots tous les jours	ผมเรียนศัพท์ใหม่สิบคำทุกวัน	".split(
+    "\t"
+  ),
+  "I learn french	J’apprends le Français	ผมเรียนภาษาฝรั่งเศส	".split("\t"),
+  "I	Je	ผม	Pom".split("\t"),
+  "To go	Aller	ไป	Pai".split("\t"),
+  "Paris	Paris	ปารีส	Paris".split("\t"),
+  "I visit Paris	Je visite paris	ผมไปปารีส	Pom pai paris".split("\t"),
+  "I learn Thai language	J’apprends le Thailandais	ผมเรียนภาษาไทย	Pom rean Pa Sa Thai".split(
+    "\t"
+  ),
+  "Tomorrow I go to France by plane	Demain je vais en france en avion	พรุ่งนี้ผมจะบินไปฝรั่งเศส	Prung nii pom ja bin pai fa-rang-sed".split(
+    "\t"
+  ),
+  "    ผ	Pho pheng".split("\t"),
+  "    ม	Mor Ma".split("\t"),
+  "    อ	Au Aang".split("\t"),
+  "    ย	Yor Yak".split("\t"),
+  "    ท	Tor Ta Harn".split("\t"),
+  "    ส	Sor Suer".split("\t"),
+  "    ว	Wo Waen".split("\t"),
+  "    ส	Sor Suer".split("\t"),
+  "I live in Switzerland	J’habite en Suisse	ผมอยู่ที่สวิส	Pom Yuu Tee Swiss".split(
+    "\t"
+  ),
+  "I want to eat at the restaurant	Je veux manger au restaurant	ผมอยากกินข้าวที่ร้านอาหาร	Pom yaag kin kao tii ran ha haan".split(
+    "\t"
+  ),
+  "I want to cook	Je veux cuisiner	ผมอยากทำอาหาร	Pom yaag tam ha haan".split("\t"),
+  "Salt	Sel	เกลือ	Gluea".split("\t"),
+  "Pepper	Poivre	พริกไทย	Phrik thai".split("\t"),
+  "To give	Donner	ให้	Haii".split("\t"),
+  "Please	S'il vous plaît		".split("\t"),
+  "Please give me salt and pepper	S'il vous plaît donnez-moi du sel et du poivre	เอาเกลือกับพริกไทยให้ผมหน่อย	Ao gluea gab phirk thai hai pom noi".split(
+    "\t"
+  ),
+  "The food is not good	Le repas n’est pas bon	อาหารไม่อร่อย	Ha haan mai aloy".split(
+    "\t"
+  ),
+  "Hello	Salut	สวัสดีครับ	Sa wa dii khrap".split("\t"),
+  "Thank you	Merci	ขอบคุณครับ	Khob khun Khrap".split("\t"),
+  "How are you?	Comment ça va?	เป็นยังไงบ้างครับ	Peng Yang Ngai Bang Krub?".split(
+    "\t"
+  ),
+  "The food is good	Le repas est bon	อร่อยมาก	A Roi Mak".split("\t"),
+  "Very spicy	Très pimenté / piquant / épicé	เผ็ดมาก	Ped mak".split("\t"),
+  "Very fun	Très rigolo / amusant	สนุกมาก	Sanook mak".split("\t"),
+  "    น	nhoh noó".split("\t"),
+  "    า	ar".split("\t"),
+  "    ร	roar ruea".split("\t"),
+  "    อัก	Aukk".split("\t"),
+  "Cute	mignon	น่ารัก	Na Ruk".split("\t"),
+  "Fried egg	Oeuf fri	ไข่ดาว	Kai Jiao".split("\t"),
+  "Minced Pork	Emincé de porc	หมูสับ	Mu Sap".split("\t"),
+  "It hurts!	Ça fait mal!	เจ็บ	Jeb-b".split("\t"),
+  "Pin hurts Louis	Pin fait mal à Louis	ทำร้าย	Pin tam-rai Louis".split("\t"),
+  "Pin’s arm is hurt	Pin a mal au bras	พิณเจ็บแขน	Pin Jeb-b Kaenn".split("\t"),
+  "headache	Mal de tête	ปวดหัว	Buad hua".split("\t"),
+  "To eat	Manger	กิน	Kin".split("\t"),
+  "What	Quoi	อะไร	A-rai".split("\t"),
+  "At / At this moment	Là / Maintenant	อยู่	Yuu".split("\t"),
+  "What are you eating now?	Que manges tu maintenant?	พิณกินอะไรอยู่	Pin Kin A-rai Yuu?".split(
+    "\t"
+  ),
+  "To like	Aimer	ชอบ	Chorb".split("\t"),
+  "To drink	Boire	ดื่ม	Duum".split("\t"),
+  "Hot	Chaud	ร้อน	Ron".split("\t"),
+  "Coffee	Café	กาแฟ	Ka fee".split("\t"),
+  "I like drinking hot coffee	J'aime boire du café chaud	ผมชอบดื่มกาแฟร้อน	Pom chorb duum ka fee ron".split(
+    "\t"
+  ),
+  "Don't like	Ne pas aimer	ไม่ชอบ	Mai chorb".split("\t"),
+  "Food	La nourriture	อาหาร	Ha haan".split("\t"),
+  "Seafood	Fruit de mer	ทะเล	Tha lee".split("\t"),
+  "I don’t like eating seafood	Je n'aime pas manger des fruits de mer	ผมไม่ชอบกินอาหารทะเล	Pom mai chorb kin hahaan talee".split(
+    "\t"
+  ),
+  "Today	Aujourd'hui	วันนี้	Wan nii".split("\t"),
+  "Work	Le travail	ทำงาน	Tham ngaan".split("\t"),
+  "Stressful	Stressant	เครียด	Kheriad".split("\t"),
+  "And	Et	และ	Lea".split("\t"),
+  "Tiring	Fatiguant	เหนื่อย	Nueay".split("\t"),
+  "Today, work is very stressful and tiring	Aujourd'hui le travail est stressant et fatiguant	วันนี้ทำงานเครียดและเหนื่อยมาก	Wan nii Tham ngaan Kheriad lea nueay maak".split(
+    "\t"
+  ),
+  "Want	Vouloir	อยาก	Yaag".split("\t"),
+  "Thai	Thailandais(e)	ไทย	Thai".split("\t"),
+  "I want to eat thai food	Je veux manger de la cuisine thailandaise	ผมอยากกินอาหารไทย	Pom yaak kin ha haan thai".split(
+    "\t"
+  ),
+  "Should	Devrais	ควร	Kouen".split("\t"),
+  "To sleep	Dormir	นอน	Norn".split("\t"),
+  "Pin should go to bed	Pin dois aller au lit	พิณควรไปนอนได้แล้วนะ	Pin kouen bai norn daii leaw na".split(
+    "\t"
+  ),
+  "Going to (for the future)	Je vais (+verbe infinitif)	กำลังจะ	Kamlang ca".split(
+    "\t"
+  ),
+  "Supermarket	Supermarché	ซุปเปอร์มาร์เก็ต	Supeer market".split("\t"),
+  "I am going to go to supermarket	Je vais aller au supermarché	ผมกำลังจะไปซุปเปอร์มาร์เก็ต	Pom Kamlang ca bai supeer market".split(
+    "\t"
+  ),
+  "Pin is the cutest	Pin est la plus mignonne	พิณน่ารักที่สุด	Pin na ruk tii sud".split(
+    "\t"
+  ),
+  "For	Pour	สำหรับ	Sam rab".split("\t"),
+  "The most	Le / la plus	ที่สุด	Tii sud".split("\t"),
+  "Pin is the most beautiful for me	Pour moi, Pin est la plus jolie	พิณสวยที่สุดสำหรับผม	Pin suai tii sud sam rab pom".split(
+    "\t"
+  ),
+  "good	Bon	ดี	Dii".split("\t"),
+  "dream	Rêves	ฝัน	Fan".split("\t"),
+  "sweet dream	Beaux rêves	ฝันดีนะ	Fan dii na".split("\t")
+];
