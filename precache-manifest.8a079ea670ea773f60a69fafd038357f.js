6deef9bd344c1d2a3c6eaa3f53a0e588f2feedb9
Louis Faucon <lpfaucon@gmail.com>
2019-05-03 17:14:49 +0200

    Updates

---
+++ precache-manifest.8a079ea670ea773f60a69fafd038357f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf0c7529f098f9581a91",
+    "url": "/flashcards/static/js/runtime~main.bf0c7529.js"
+  },
+  {
+    "revision": "fc80a9f3380d7edcd343",
+    "url": "/flashcards/static/js/main.fc80a9f3.chunk.js"
+  },
+  {
+    "revision": "8f3bee8a17ae5b1b0056",
+    "url": "/flashcards/static/js/1.8f3bee8a.chunk.js"
+  },
+  {
+    "revision": "fc80a9f3380d7edcd343",
+    "url": "/flashcards/static/css/main.a3f011e2.chunk.css"
+  },
+  {
+    "revision": "5355b7f054a643195812d68618ce27cb",
+    "url": "/flashcards/index.html"
+  }
+];
